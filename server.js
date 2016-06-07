5d58b230f19af08b0151deb9910c4883da42f34d
Justin Bennett <fortitude1981@gmail.com>
2016-06-07 13:42:54 -0500

    prep repo for heroku

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express');  //access to entire express api
 
 //create app
 var app = express();
+const PORT = process.env.PORT || 3000;  //now heroku can pass to the PORT
+
+
+//express middleware, direct https traffic to http
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();  //request process, return file to browser
+  }else{
+    res.redirect('http://' + req.hostname + req.url); //redirect here
+  }
+});
 
 app.use(express.static('public'));  //which folder we wanna serve
 
-app.listen(3000, function() {       //start server
-  console.log('express server is up on port 3000')
+app.listen(PORT, function() {       //start server
+  console.log('express server is up on port ' + PORT);
 });
